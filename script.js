5674eed8111064d463a8037cd6e8cb17ef65717d
ShakirovIlnur <maksimpetrov032018@gmail.com>
2025-09-17 23:07:23 +0900

    adden dop work

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-let name = prompt("Ваше имя");
-
-alert("Привет, " + name);
+let number = prompt("Загадайте любое число");
+alert(number1 = + number * 2);
+alert(number2 = number1 + 10);
+alert(number3 = number2 / 2);
+alert(number4 = number3 - number);
+alert("число равно " + 5);
